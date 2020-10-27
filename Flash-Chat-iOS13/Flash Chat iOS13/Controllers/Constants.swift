62b283d587276908c32087f96a06a4eafc405988
kartikeyachaauhan <kc1.408596@gmail.com>
2020-10-28 00:02:06 +0530

    add constants file

---
+++ Flash-Chat-iOS13/Flash Chat iOS13/Controllers/Constants.swift
@@ -0,0 +1,29 @@
+//
+//  Constants.swift
+//  Flash Chat iOS13
+//
+//  Created by Kartikeya Chauhan on 27/10/20.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+struct K {
+    static let appName = "⚡️FlashChat"
+    static let registerSegue = "RegisterToChat"
+    static let loginSegue = "LoginToChat"
+    static let cellIdentifier = "ReusableCell"
+    static let cellNibName = "MessageCell"
+    
+    struct BrandColors {
+        static let purple = "BrandPurple"
+        static let lightPurple = "BrandLightPurple"
+        static let blue = "BrandBlue"
+        static let lighBlue = "BrandLightBlue"
+    }
+    
+    struct FStore {
+        static let collectionName = "messages"
+        static let senderField = "sender"
+        static let bodyField = "body"
+        static let dateField = "date"
+    }
+}
