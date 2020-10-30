3e2de7ad9acfb86c7b68857e35e4e55b1543c2d3
kartikeyachaauhan <kc1.408596@gmail.com>
2020-10-30 17:38:36 +0530

    add chat view and message model

---
+++ Flash-Chat-iOS13/Flash Chat iOS13/Controllers/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Kartikeya Chauhan on 30/10/20.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+}
